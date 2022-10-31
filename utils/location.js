810526fd2cd35c1350fefc66f0e1aef50f2398ca
Tim Kent <timdekent@gmail.com>
2022-10-30 20:32:10 -0700

    move geolocate get request to frontend

--- utils/location.js
+++ utils/location.js
@@ -1,16 +1,16 @@
-const axios = require('axios')
+// const axios = require('axios')
 
-const HttpError = require('../models/http-error')
-const { googleGeoKey } = require('../config/config')
+// const HttpError = require('../models/http-error')
+// const { googleGeoKey } = require('../config/config')
 
-const getCoordsForAddress = async (address) => {
-  const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${googleGeoKey}`)
-  const data = response.data
-  if (!data || data.response === 'ZERO_RESULTS') {
-    const error = HttpError('Could not find location for the specified address!', 404)
-    throw error
-  }
-  return data.results[0].geometry.location
-}
+// const getCoordsForAddress = async (address) => {
+//   const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${googleGeoKey}`)
+//   const data = response.data
+//   if (!data || data.response === 'ZERO_RESULTS') {
+//     const error = HttpError('Could not find location for the specified address!', 404)
+//     throw error
+//   }
+//   return data.results[0].geometry.location
+// }
 
-module.exports = getCoordsForAddress
+// module.exports = getCoordsForAddress
